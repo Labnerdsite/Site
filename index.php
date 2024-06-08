515834ba38836abb5457d62ec6326a8bbdd2f5b5
Labnerdsite <projetolabnerd@gmail.com>
2024-06-08 17:11:51 -0300

    Criar página index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Labnerd</title>
+<style type="text/css">
+body {
+    background-color: #E7E7E7;
+}
+</style>
+</head>
+
+<body style="text-align: center">
+<table width="908" border="0" align="center" cellpadding="3">
+  <tbody>
+    <tr>
+      <td height="200" colspan="3" align="center"><img src="img/__!CAPA!.jpg" width="900" height="200" alt=""/></td>
+    </tr>
+    <tr>
+      <td height="6" colspan="3" align="center" bgcolor="#FFFFFF"><a href="http://www.labnerd.rf.gd"><img src="img/P_1!.jpg" width="900" height="30" alt=""/></a></td>
+    </tr>
+    <tr>
+      <td width="295" height="7" align="center" bgcolor="#FFFFFF"><a href="http://www.labnerd.rf.gd/sejamembro.php"><img src="img/P_SEJA_MEMBRO!.jpg" width="295" height="35" alt=""/></a></td>
+      <td width="295" align="center" bgcolor="#FFFFFF"><a href="http://www.labnerd.rf.gd/analises.php"><img src="img/P_2!.jpg" width="295" height="30" alt=""/></a></td>
+      <td width="295" align="center" bgcolor="#FFFFFF"><a href="http://www.labnerd.rf.gd/apoio.php"><img src="img/P_APOIE!.jpg" width="295" height="35" alt=""/></a><a href="https://mpago.la/25Z71j3"></a></td>
+    </tr>
+    <tr>
+      <td height="585" colspan="3" align="center" bgcolor="#FFFFFF"><table width="406" border="0" align="center" cellpadding="3">
+          <tbody>
+          <tr>
+            <td width="396" height="341" align="center"><table width="393" border="0" align="center">
+              <tbody>
+                <tr>
+                  <td width="387" height="358" align="center"><img src="img/banner2!.jpg" width="650" height="450" alt=""/></td>
+                  </tr>
+                </tbody>
+            </table></td>
+            </tr>
+          </tbody>
+    </table></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
